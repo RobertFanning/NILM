66dd711260c8ae741597574189de6c5fdd78a37f
RobertFanning <33347098+RobertFanning@users.noreply.github.com>
2018-01-15 16:14:56 +0100

    Create includes.h

---
+++ includes.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+ **
+ **
+ **    Master inlude file
+ **
+ **    Used with ARM IAR C/C++ Compiler
+ **
+ **    (c) Copyright IAR Systems 2007
+ **
+ **    $Revision: 28 $
+ **
+ ***************************************************************************/
+
+#ifndef __INCLUDES_H
+#define __INCLUDES_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <intrinsics.h>
+#include <assert.h>
+#include <nxp/iolpc2478.h>
+
+#include "arm_comm.h"
+#include "board.h"
+
+#include "usb_cnfg.h"
+#include "usb_desc.h"
+#include "usb_hw.h"
+#include "usb_t9.h"
+#include "usb_hooks.h"
+#include "usb_dev_desc.h"
+#include "usb_hooks.h"
+#include "usb_buffer.h"
+#include "functions.h"
+#include "drv_touch_scr.h"
+#include "menu.h"
+#include "cursor_arrow.h"
+
+#include "cd_class.h"
+#include "cdc_desc.h"
+#include "cdc_cmd.h"
+#include "timer.h"
+#include "uart.h"
+#include "clock.h"
+#include "clock-arch.h"
+
+#include "sys.h"
+#include "sdram_64M_32bit_drv.h"
+#include "drv_glcd.h"
+#include "iar_logo.h"
+#include "Background.h"
+#endif  // __INCLUDES_H
