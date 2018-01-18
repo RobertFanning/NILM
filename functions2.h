b75374e49742a3152cd8052a77b7d1f6f3b78d49
RobertFanning <33347098+RobertFanning@users.noreply.github.com>
2018-01-18 14:07:39 +0100

    Create functions2.h

---
+++ functions2.h
@@ -0,0 +1,47 @@
+/*************************************************************************
+ *
+ *    Used with ICCARM and AARM.
+ *
+ *    (c) Copyright IAR Systems 2006
+ *
+ *    File name   : uart.h
+ *    Description : UARTs module header
+ *
+ *    History :
+ *    1. Date        : July 6, 2006
+ *       Author      : Stanimir Bonev
+ *       Description : Create
+ *
+ *    $Revision: 4288 $
+ **************************************************************************/
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+#include "includes.h"
+#include "uart.h"
+
+/*************************************************************************
+ * Function Name: Uart0Isr
+ * Parameters: none
+ *
+ * Return: none
+ *
+ * Description: UART 0 interrupt routine
+ *
+ *************************************************************************/
+struct measurements {double V; double A; double P; double Q; double PF;};
+typedef struct measurements RS232;
+struct device {char name[10]; Boolean status; double P; double Q; double PF;};
+typedef struct device Device;
+
+double convVolt(pInt8U Buffer);
+double convAmp(pInt8U Buffer);
+double convPow(pInt8U Buffer);
+double convPF(pInt8U Buffer);
+double convPowR(pInt8U Buffer);
+void shiftPrevious(RS232 previous[]);
+int EdgeDetect(RS232 previous[], double TotalP);
+void FindMatch(RS232 previous[],Device devices[], int DevicesRegistered, int DeviceID[]);
+Boolean StabilityCheck(RS232 previous[]);
+void shift_power(int power[]);
+
+#endif
